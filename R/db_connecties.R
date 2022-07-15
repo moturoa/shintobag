25422c4922490363e295acf8149db2e7454a4276
Remko Duursma <remko@shintolabs.nl>
2022-07-15 11:12:30 +0200

    config management

--- R/db_connecties.R
+++ R/db_connecties.R
@@ -1,14 +1,18 @@
 
 #' Make a database connection
 #' @description Make a connection with RPostgres to one of the Shinto Labs databases.
+#' @details `global_db_connection` is a shorthand for `shinto_db_connection` with `search_global = TRUE`. 
+#' Use this option to store encrypted passwords in a
 #' @param what Welke database connectie in het config bestand? (bv. "BAGdata")
 #' @param file Lokatie van het config bestand, normaal gezet via `options(shintobag_conf = /path/to/file)`
+#' @param search_global If TRUE, ignores 'file' argument and tries to find global config file.
 #' @param port_default The default port if missing from config ("dbport")
 #' @param pool Logical. Als TRUE, gebruikt `pool::dbPool`, anders `DBI::dbConnect`
-#' @export
+#' @export 
+#' @rdname shinto_db_connection
 shinto_db_connection <- function(what,
                                  file = getOption("shintobag_conf", "conf/config.yml"),
-                                 port_default = 5432,
+                                 search_global = FALSE,
                                  allow_default_fallback = FALSE,
                                  pool = FALSE,
                                  config_entry = NULL){
@@ -17,39 +21,60 @@ shinto_db_connection <- function(what,
     config_entry <- Sys.getenv("R_CONFIG_ACTIVE", "default")  
   }
 
+  if(search_global){
+    env <- get_global_config_filename()
+    if(env == ""){
+      message("SHINTO_GLOBAL_DB_CONFIG environment variable not set; continuing with standard config file")
+    } else {
+      file <- open_global_config()  
+    }
+  }
+  
   conf <- config::get(value = what,
                       config = config_entry,
                       file = file)
 
+  # If password is encrypted, decrypt it before connecting
+  if(string_is_encrypted(conf$dbpassword)){
+    conf$dbpassword <- decrypt(conf$dbpassword)
+  }
 
   # Als NULL in een non-default, zoek in default, alleen als allowed
   if(allow_default_fallback & is.null(conf) & config_entry != "default"){
-    conf <- config::get(value = what,
-                        config = "default",
-                        file = file)
+    # conf <- config::get(value = what,
+    #                     config = "default",
+    #                     file = file)
+    warning("'allow_default_fallback' argument is deprecated.")
   }
 
   if(is.null(conf)){
     stop(paste("Entry", what, "not found in", file," - add password information and try again!"))
   }
 
-  port <- ifelse(is.null(conf$dbport), default_port, conf$dbport)
-
   if(!pool){
     DBI::dbConnect(RPostgres::Postgres(),
                    dbname = conf$dbname,
                    host = conf$dbhost,
-                   port = port,
+                   port = conf$dbport,
                    user = conf$dbuser,
                    password = conf$dbpassword)
   } else {
     pool::dbPool(RPostgres::Postgres(),
                  dbname = conf$dbname,
                  host = conf$dbhost,
-                 port = port,
+                 port = conf$dbport,
                  user = conf$dbuser,
                  password = conf$dbpassword)
   }
 
+}
+
+
+
+
+#' @export
+#' @rdname shinto_db_connection
+global_db_connection <- function(...){
+  shinto_db_connection(..., global_search = TRUE)  
 }
 
---
+++ R/global_config.R
@@ -0,0 +1,123 @@
+#' Reads a simple YAML with required DB connections and adds them to the global config
+#' @param db_config Path to YAML file with simple DB entries (new format!)
+#' @export
+add_db_connections_to_global <- function(db_config){
+  
+  glob_config_file <- open_global_config()
+  lis <- read_db_connections_yaml(db_config)
+  
+  add_section <- function(section){
+    dev <- lis[[section]]
+    for(i in seq_along(dev)){
+      add_config_entry(name = names(dev)[i], 
+                       dbname = dev[[i]][1], 
+                       dbuser = if(length(dev[[i]]) == 1)dev[[i]][1] else dev[[i]][2], 
+                       where = section,
+                       encrypt = TRUE, file = glob_config_file)
+    }
+  }
+  
+  # TODO might be other sections as well like 'productionlocal'
+  add_section("development")
+  add_section("production")
+  
+}
+
+get_global_config_filename <- function(){
+  Sys.getenv("SHINTO_GLOBAL_DB_CONFIG")
+}
+
+open_global_config <- function(){
+  
+  fn <- get_global_config_filename()
+  
+  ext <- tools::file_ext(fn)
+  if(!ext %in% c("yaml","yml")){
+    stop("config filename extension must be 'yaml' or 'yml'")
+  }
+  
+  if(fn == ""){
+    stop(paste("Set the environment variable SHINTO_GLOBAL_DB_CONFIG to your global shinto db config file.",
+               "If it does not exist, it will be created for you."))
+  }
+  
+  if(!file.exists(fn)){
+    dir.create(dirname(fn), showWarnings = FALSE)
+    writeLines("", fn)
+  } else {
+    tm <- try(yaml::read_yaml(fn))
+    if(inherits(tm, "try-error")){
+      stop("Global config found but could not be read. Malformed YAML?")
+    }
+  }
+  
+  return(fn)
+  
+}
+
+
+
+
+read_db_connections_yaml <- function(filename){
+  
+  if(!file.exists(filename)){
+    stop(paste(filename, "not found"))
+  }
+  
+  tm <- try(yaml::read_yaml(filename))
+  if(inherits(tm, "try-error")){
+    stop("Global config found but could not be read. Malformed YAML?")
+  }
+  
+  tm
+  
+}
+
+
+
+
+
+
+
+
+if(FALSE){
+  
+  
+  Sys.setenv(SHINTO_GLOBAL_DB_CONFIG = "c:/Users/RemkoDuursma/.shinto/config.yaml")
+  Sys.setenv(SHINTO_PASS_SECRET = "irubnfgobfgpobmfgpmwediwyqw3pevdf")
+  Sys.setenv(SHINTO_DEV2_LOCAL_PORT = 2222)
+  
+  
+  
+  open_global_config()
+  
+  
+  
+  x <- read_db_connections_yaml("test/db_connections.yaml")
+  
+  
+  
+  glob_config_file <- open_global_config()
+  glob_config <- read_config(glob_config_file)
+  
+  
+  has_config_entry("Eindhoven", glob_config, "development")
+  
+  
+  
+  config_is_encrypted(glob_config_file)
+  
+  
+  
+  add_db_connections_to_global("test/db_connections.yaml")
+  
+  x <- yaml::read_yaml(open_global_config())
+  
+  
+  
+  con <- shinto_db_connection("Eindhoven", search_global = TRUE)
+  
+  
+  
+}
+
