291a67fbd9fecf15f164cc7bf1336bc96b4cbf2c
Remko Duursma <remko@shintolabs.nl>
2022-10-14 13:18:32 +0200

    fix

--- R/get_cache_path.R
+++ R/get_cache_path.R
@@ -2,7 +2,7 @@
 #' @param cache The local cache, normally 'cache'
 #' @export
 get_bag_cache_path <- function(cache = "cache"){
-  ifelse(Sys.getenv("R_CONNECT_SERVER") == "",  # env var only on rsconnect
+  ifelse(Sys.getenv("CONNECT_SERVER") == "",  # env var only on rsconnect
          cache, 
          "/data/BAG")   # path op rstudio connect (op devapp althans)  
 }
