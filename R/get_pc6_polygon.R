86dba4bd113e183a694d1a78882c0db5e0e24d5b
martijnheijstek <martijn@shintolabs.nl>
2022-11-22 13:51:45 +0100

    altered pc6 query

--- R/get_pc6_polygon.R
+++ R/get_pc6_polygon.R
@@ -2,7 +2,8 @@
 #' @param con Connection to CBS database. If missing looks for `data_cbs` connection in config.yml 
 #' @param geovlak An sfc polygon (geo column in an sf dataframe)
 #' @export
-get_pc6_polygon <- function(geovlak, con = NULL,  ...){
+get_pc6_polygon <- function(geovlak, con = NULL,  gemeenten=NULL, ...){
+  
   
   if(is.null(con)){
     con <- shinto_db_connection("data_cbs", ...)
@@ -14,9 +15,12 @@ get_pc6_polygon <- function(geovlak, con = NULL,  ...){
   
   polygon_txt <- sf::st_as_text(polygon)  
   
+  # increase performance by subsetting region
+  subsetquery <- ifelse(!is.null(gemeenten), glue("in ('{paste(gemeenten, collapse=\"','\")}') and "), "")
+  
   # get data in polygon
   sf::st_read(con, query = glue::glue("select * from cbs_postcode6_2020 as geodata ",
-                                " where st_contains(ST_GeomFromText('{polygon_txt}', 28992),",
+                                " where {subsetquery}st_contains(ST_GeomFromText('{polygon_txt}', 28992),",
                                 " st_centroid(geodata.geometry))")) %>% 
     sf::st_transform(4326)
   
